754f12a1c944b02d4ae0c63468b55120e58a1171
liber09 <linda.bergsangel@hotmail.com>
2023-02-03 17:28:49 +0100

    Added firebsase to app and user mock data

---
+++ app/src/main/java/com/example/feedme/data/User.kt
@@ -0,0 +1,15 @@
+package com.example.feedme.data
+
+data class User (
+    val firstName: String = "",
+    val lastName: String = "",
+    val address: String = "",
+    val city: String = "",
+    val postalCode: String = "",
+    val phoneNumber: String = "",
+    val eMail: String = "",
+
+
+
+        )
+
