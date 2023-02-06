023bc991c68fe0c97c599869cd769c1841d62226
liber09 <linda.bergsangel@hotmail.com>
2023-02-06 19:14:06 +0100

    Display data in recyclerView

---
+++ app/src/main/java/com/example/feedme/ShoppingCartReyclerAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.feedme
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.feedme.data.Cart
+import com.google.android.material.chip.Chip
+
+class ShoppingCartReyclerAdapter(val context: Context, val shoppingCartItems : List<Cart>): RecyclerView.Adapter<ShoppingCartReyclerAdapter.ViewHolder>() {
+
+    var layoutInflater = LayoutInflater.from(context)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView = layoutInflater.inflate(R.layout.list_item_shopping_cart, parent,false)
+        return ViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val cartItems = shoppingCartItems[position]
+        holder.cartItemRowCount.text = cartItems.rowCount.toString()
+        holder.cartItemRowTitle.text = cartItems.rowTitle
+        holder.cartItemRowAllergen.text = cartItems.rowAllergen
+        holder.cartItemRowPrice.text = cartItems.rowPrice.toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return shoppingCartItems.size
+    }
+
+
+    inner class  ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val cartItemRowCount = itemView.findViewById<TextView>(R.id.textViewCartRowCount)
+        val cartItemRowAllergen = itemView.findViewById<Chip>(R.id.chipCartRowItemAlergen)
+        val cartItemRowTitle = itemView.findViewById<TextView>(R.id.textViewCartRowItemTitle)
+        val cartItemRowPrice = itemView.findViewById<TextView>(R.id.textViewCartItemRowPrice)
+        var cartDisplayPosition = 0
+
+
+        init {
+
+        }
+    }
+}
