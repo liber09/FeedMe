852deed9fc9256bd127d148489d7a9c2a6adae0e
liber09 <linda.bergsangel@hotmail.com>
2023-02-06 12:38:30 +0100

    building shoping cart layout

---
+++ app/src/main/java/com/example/feedme/ShoppingCart.kt
@@ -0,0 +1,11 @@
+package com.example.feedme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShoppingCart : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_cart)
+    }
+}
