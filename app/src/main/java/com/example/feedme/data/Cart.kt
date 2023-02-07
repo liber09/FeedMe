fe7b612729bd2fa3570fc3a517c4234289e1deab
liber09 <linda.bergsangel@hotmail.com>
2023-02-07 22:00:07 +0100

    layout completed

--- app/src/main/java/com/example/feedme/data/Cart.kt
+++ app/src/main/java/com/example/feedme/data/Cart.kt
@@ -1,7 +1,7 @@
 package com.example.feedme.data
 
 data class Cart(
-    var rowCount:Int = 0,
+    var rowCount: Int = 0,
     val rowTitle: String = "",
     val rowAllergen: String = "",
     var rowPrice: Double = 0.0,
