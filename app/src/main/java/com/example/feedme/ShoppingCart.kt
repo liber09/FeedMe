0ce245ed460b44ea3f675388995cbbf311293edc
liber09 <linda.bergsangel@hotmail.com>
2023-02-07 05:50:24 +0100

    Display allergenes in shoppingCart

--- app/src/main/java/com/example/feedme/ShoppingCart.kt
+++ app/src/main/java/com/example/feedme/ShoppingCart.kt
@@ -2,10 +2,29 @@ package com.example.feedme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.feedme.data.Cart
+
+var shoppingCartItems: MutableList<Dishes> = arrayListOf()
+
 
 class ShoppingCart : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shopping_cart)
     }
+
+    fun addToCart(item:Dishes){
+        var cartItem = item.title?.let {
+            Cart(
+                rowCount = 1,
+                rowTitle = item.title!!,
+                rowAllergen = item.isEggFree.toString()
+            )
+        }
+    }
+
+    fun removeFromCart(item:Dishes){
+        shoppingCartItems
+    }
+
 }
--- app/src/main/java/com/example/feedme/data/Cart.kt
+++ app/src/main/java/com/example/feedme/data/Cart.kt
@@ -4,5 +4,11 @@ data class Cart(
     var rowCount:Int = 0,
     val rowTitle: String = "",
     val rowAllergen: String = "",
-    var rowPrice: Double = 0.0
+    var rowPrice: Double = 0.0,
+    val rowIsGlutenFree: Boolean = false,
+    val rowIslactoseFree: Boolean = false,
+    var rowIsEggFree: Boolean = false,
+    var rowIsNutfree: Boolean= false,
+    var rowIsFreeFromSeeFood: Boolean = false,
+    var rowIsSoyfree: Boolean = false
 )
