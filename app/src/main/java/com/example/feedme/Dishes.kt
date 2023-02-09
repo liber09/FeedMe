f0557cc5ad72b30486a320727dce73ef3506db61
liber09 <linda.bergsangel@hotmail.com>
2023-02-09 22:38:24 +0100

    Sometimes i wonder if i am insane or if it's android studio

---
+++ app/src/main/java/com/example/feedme/Dishes.kt
@@ -0,0 +1,35 @@
+package com.example.feedme.data
+
+import com.google.firebase.firestore.DocumentId
+
+data class Dishes(
+    var title: String? = null,
+    var description: String? = null,
+    var priceNormalPortion: Double? = null,
+    var priceSmallPortion: Double? = null,
+    var priceLargePortion: Double? = null,
+    var isGlutenFree: Boolean = false,
+    var isLaktoseFree: Boolean = false,
+    var isEggFree: Boolean = false,
+    var isNutfree: Boolean= false,
+    var isFreeFromSeeFood: Boolean = false,
+    var isSoyfree: Boolean = false,
+    var isLCHF: Boolean = false,
+    var isVegetarian: Boolean = false,
+    var isVegan: Boolean = false,
+    var category: String = "Huvudrätt", // dropdownfält?
+    var canBeMadeGlutenFree: Boolean = false,
+    var canBeMadeLaktosFree: Boolean = false,
+    var canBeMadeVegan: Boolean = false,
+    var canBeMadeVegeterian: Boolean = false,
+    var extraCostGluten: Double? = null,
+    var extraCostLaktose: Double? = null,
+    var extraCostVegan: Double? = null,
+    var extraCostVegeterian: Double? = null,
+    var selectedFoodSize : String = "",
+    var dishImagePath :String = "",
+    var count: Int = 0,
+    @DocumentId var documentId : String? = null,
+    @DocumentId var restaurantDocumentId : String? = null,
+
+    )
