cec7ca0eddef37a6c94d05ef8d941f5b9935ccee
tibalomium <ninjaharry@gmail.com>
2023-02-15 20:32:38 +0100

    Det är bara resten kvar

--- app/src/main/java/com/example/feedme/InfoRestaurantActivity.kt
+++ app/src/main/java/com/example/feedme/InfoRestaurantActivity.kt
@@ -2,7 +2,6 @@ package com.example.feedme
 
 import android.app.TimePickerDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.CheckBox
@@ -12,7 +11,6 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.forEach
 import com.example.feedme.data.Restaurant
 import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
@@ -22,7 +20,6 @@ import kotlin.collections.HashMap
 class InfoRestaurantActivity : AppCompatActivity() {
 
     val db = Firebase.firestore
-    val registerNew = false
     private var openingHours = hashMapOf<String, Date>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,7 +29,6 @@ class InfoRestaurantActivity : AppCompatActivity() {
         val btnSave = findViewById<Button>(R.id.btn_save)
         val btnAddImage = findViewById<Button>(R.id.btn_add_image)
         //val loadRestaurant = intent.getStringExtra("restaurantToLoad")
-        //loadRestaurant("asdf")
 
         val restaurantRef = db.collection("restaurantTibTest")
         restaurantRef.addSnapshotListener{ snapshot, e ->
@@ -79,49 +75,7 @@ class InfoRestaurantActivity : AppCompatActivity() {
         }
     }
 
-    fun loadRestaurant(restaurant: Restaurant) {
-        Log.v("!!!", restaurant.name)
-        findViewById<EditText>(R.id.textInputName).setText(restaurant.name)
-        findViewById<EditText>(R.id.textInputAddress).setText(restaurant.address)
-        findViewById<EditText>(R.id.textInputPostalCode).setText(restaurant.postalCode)
-        findViewById<EditText>(R.id.textInputCity).setText(restaurant.city)
-        findViewById<EditText>(R.id.textInputPhone).setText(restaurant.phoneNumber)
-        findViewById<EditText>(R.id.textInputEmail).setText(restaurant.eMail)
-        setType(restaurant.type ?: "")
-        findViewById<EditText>(R.id.textInputDeliveryPrice).setText(restaurant.deliveryFee.toString() ?: "0")
-        findViewById<CheckBox>(R.id.cb_takeaway).isChecked = restaurant.deliveryTypePickup ?: false
-        findViewById<CheckBox>(R.id.cb_homeDelivery).isChecked = restaurant.deliveryTypeHome ?: false
-        findViewById<CheckBox>(R.id.cb_atRestaurant).isChecked = restaurant.deliveryTypeAtRestaurant ?: false
-        findViewById<CheckBox>(R.id.cb_tableBooking).isChecked = restaurant.tableBooking ?: false
-        loadOpeningHours(restaurant.openingHours)
-        findViewById<EditText>(R.id.textInputDescription).setText(restaurant.description ?: "")
-    }
-
-    fun setTime(view: View) {
-
-        if(view is EditText) {
-            val cal = Calendar.getInstance()
-            cal.time = Date()
-            //val date = Date()
-
-            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
-                cal.set(Calendar.HOUR_OF_DAY, hour)
-                cal.set(Calendar.MINUTE, minute)
-
-                view.setText(SimpleDateFormat("HH:mm").format(cal.time))
-                openingHours.put(view.tag.toString(), cal.time)
-            }
-
-            TimePickerDialog(
-                this,
-                timeSetListener,
-                cal.get(Calendar.HOUR_OF_DAY),
-                cal.get(Calendar.MINUTE),
-                true
-            ).show()
-        }
-    }
-
+    //Need to be able to update values
     fun saveInfo() {
         val name = findViewById<EditText>(R.id.textInputName).text.toString()
 
@@ -150,18 +104,46 @@ class InfoRestaurantActivity : AppCompatActivity() {
             .set(rest)
     }
 
-    //not optimized version
-    fun setType(types: String) {
-        var listOfTypes = types.split(" ")
+    fun loadRestaurant(restaurant: Restaurant) {
+        findViewById<EditText>(R.id.textInputName).setText(restaurant.name)
+        findViewById<EditText>(R.id.textInputAddress).setText(restaurant.address)
+        findViewById<EditText>(R.id.textInputPostalCode).setText(restaurant.postalCode)
+        findViewById<EditText>(R.id.textInputCity).setText(restaurant.city)
+        findViewById<EditText>(R.id.textInputPhone).setText(restaurant.phoneNumber)
+        findViewById<EditText>(R.id.textInputEmail).setText(restaurant.eMail)
+        setType(restaurant.type ?: "")
+        findViewById<EditText>(R.id.textInputDeliveryPrice).setText(restaurant.deliveryFee.toString() ?: "0")
+        findViewById<CheckBox>(R.id.cb_takeaway).isChecked = restaurant.deliveryTypePickup ?: false
+        findViewById<CheckBox>(R.id.cb_homeDelivery).isChecked = restaurant.deliveryTypeHome ?: false
+        findViewById<CheckBox>(R.id.cb_atRestaurant).isChecked = restaurant.deliveryTypeAtRestaurant ?: false
+        findViewById<CheckBox>(R.id.cb_tableBooking).isChecked = restaurant.tableBooking ?: false
+        openingHours = restaurant.openingHours
+        loadOpeningHours(restaurant.openingHours)
+        findViewById<EditText>(R.id.textInputDescription).setText(restaurant.description ?: "")
+    }
 
-        val mView = findViewById<ConstraintLayout>(R.id.mainView)
+    fun setOpeningHours(view: View) {
 
-        mView.forEach { cb ->
-            if(cb is CheckBox && cb.tag?.toString() == "type" && listOfTypes.contains(cb.text.toString())) {
-                cb.isChecked = true
+        if(view is EditText) {
+            val cal = Calendar.getInstance()
+            //cal.time = Date()
+
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY, hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                view.setText(SimpleDateFormat("HH:mm").format(cal.time))
+                openingHours.put(view.tag.toString(), cal.time)
             }
-        }
 
+            TimePickerDialog(
+                this,
+                timeSetListener,
+                cal.get(Calendar.HOUR_OF_DAY),
+                cal.get(Calendar.MINUTE),
+                true
+            ).show()
+        }
     }
 
     fun loadOpeningHours(oHours: HashMap<String, Date>) {
@@ -173,6 +155,19 @@ class InfoRestaurantActivity : AppCompatActivity() {
                 val date = oHours.get(et.tag.toString()) as com.google.firebase.Timestamp
                 cal.time = date.toDate()
                 et.setText(SimpleDateFormat("HH:mm").format(cal?.time))
+            }
+        }
+    }
+
+    //not optimized version
+    fun setType(types: String) {
+        var listOfTypes = types.split(" ")
+
+        val mView = findViewById<ConstraintLayout>(R.id.mainView)
+
+        mView.forEach { cb ->
+            if(cb is CheckBox && cb.tag?.toString() == "type" && listOfTypes.contains(cb.text.toString())) {
+                cb.isChecked = true
             }
         }
     }
