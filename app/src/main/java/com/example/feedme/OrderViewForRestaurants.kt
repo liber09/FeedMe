8d9ecc52587ce7a41cd72c37e1544ba1dbf22d3e
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-02-15 10:16:44 +0100

    Revert "Revert "Get restaurant orders from db""

--- app/src/main/java/com/example/feedme/OrderViewForRestaurants.kt
+++ app/src/main/java/com/example/feedme/OrderViewForRestaurants.kt
@@ -2,10 +2,23 @@ package com.example.feedme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
+lateinit var recyclerViewRestaurantOrders : RecyclerView
 class OrderViewForRestaurants : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_view_for_restaurants)
+
+        recyclerViewRestaurantOrders = findViewById<RecyclerView>(R.id.RvRestaurantOrders)
+        recyclerViewShoppingCart.layoutManager= LinearLayoutManager(this)
+        val adapter = RestaurantOrdersRVAdapter(this,DataManagerOrders.orders)
+        recyclerViewRestaurantOrders.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        recyclerViewRestaurantOrders.adapter?.notifyDataSetChanged()
     }
 }
