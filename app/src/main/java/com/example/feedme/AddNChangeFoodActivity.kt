64f0b65f9cdce0b675c0f13627adfac7b5c12fd4
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-02-21 21:03:09 +0100

    editing works, now right restaurant ref

--- app/src/main/java/com/example/feedme/AddNChangeFoodActivity.kt
+++ app/src/main/java/com/example/feedme/AddNChangeFoodActivity.kt
@@ -147,7 +147,7 @@ class AddNChangeFoodActivity : AppCompatActivity() {
                 .into( dishImage)
         }
 
-        //nedan funkar inte fullt ut det är inte inbockat
+
 
         if (dish.isGlutenFree == true) {
             isGlutenFreeCB.isChecked = true
@@ -215,9 +215,6 @@ class AddNChangeFoodActivity : AppCompatActivity() {
     fun EditDish(position: Int){
 
        // val restaurant = auth.currentUser
-
-
-
         DataManagerDishes.dishes[position].title =dishNameET.text.toString()
         DataManagerDishes.dishes[position].description = descriptionET.text.toString()
         DataManagerDishes.dishes[position].isGlutenFree = false
@@ -273,40 +270,42 @@ class AddNChangeFoodActivity : AppCompatActivity() {
         if(canBeMadeLaktoseFreeCB.isChecked){
             DataManagerDishes.dishes[position].canBeMadeLaktosFree = true
         }
-        DataManagerDishes.dishes[position].extraCostVegan = 0.0
-        if (veganExtraCostET.text.toString().toDouble() <= 0.0 && veganExtraCostET.text.isNotEmpty()) {
+       // DataManagerDishes.dishes[position].extraCostVegan == null
+        if (veganExtraCostET.text.toString().isNotEmpty() ) {
             DataManagerDishes.dishes[position].extraCostVegan =veganExtraCostET.text.toString().toDouble()
-        }
+        } else {DataManagerDishes.dishes[position].extraCostVegan = null}
 
-        DataManagerDishes.dishes[position].extraCostVegeterian =  0.0
-        if (vegeterianExtraCostET.text.toString().toDouble() <= 0.0 && vegeterianExtraCostET.text.isNotEmpty() ) {
+      //  DataManagerDishes.dishes[position].extraCostVegeterian ==  null
+        if (vegeterianExtraCostET.text.toString().isNotEmpty() ) {
             DataManagerDishes.dishes[position].extraCostVegeterian = vegeterianExtraCostET.text.toString().toDouble()
         }
 
-        DataManagerDishes.dishes[position].extraCostGluten =  0.0
-        if (glutenExtraCostET.text.toString().toDouble() <= 0.0 && glutenExtraCostET.text.isNotEmpty()) {
+       // DataManagerDishes.dishes[position].extraCostGluten ==  null
+        if (glutenExtraCostET.text.toString().isNotEmpty()) {
             DataManagerDishes.dishes[position].extraCostVegeterian = glutenExtraCostET.text.toString().toDouble()
 
         }
 
-        DataManagerDishes.dishes[position].extraCostLaktose =  0.0
-        if (laktosExtraCostET.text.toString().toDouble() <= 0.0 && laktosExtraCostET.text.isNotEmpty() ) {
+       // DataManagerDishes.dishes[position].extraCostLaktose ==  null
+        if (laktosExtraCostET.text.toString().isNotEmpty() ) {
             DataManagerDishes.dishes[position].extraCostLaktose =laktosExtraCostET.text.toString().toDouble()
         }
 
-        DataManagerDishes.dishes[position].priceSmallPortion =  0.0
-        if (smalPriceET.text.toString().toDouble() <= 0.0 && smalPriceET.text.isNotEmpty() ) {
+       // DataManagerDishes.dishes[position].priceSmallPortion ==  null
+        if (smalPriceET.text.toString().isNotEmpty() ) {
             DataManagerDishes.dishes[position].priceSmallPortion = smalPriceET.text.toString().toDouble()
         }
-        DataManagerDishes.dishes[position].priceLargePortion =  0.0
-        if (largePriceET.text.toString().toDouble() <= 0.0 &&largePriceET.text.isNotEmpty()) {
+      //  DataManagerDishes.dishes[position].priceLargePortion ==  null
+        if (largePriceET.text.toString().isNotEmpty()) {
             DataManagerDishes.dishes[position].priceLargePortion = largePriceET.text.toString().toDouble()
         }
-        DataManagerDishes.dishes[position].priceNormalPortion = 0.0
+       // DataManagerDishes.dishes[position].priceNormalPortion == null
 
-        if (normalPriceET.text.toString().toDouble() <= 0.0 && normalPriceET.text.isNotEmpty() ) {
+        if (normalPriceET.text.toString().isNotEmpty() ) {
             DataManagerDishes.dishes[position].priceNormalPortion = normalPriceET.text.toString().toDouble()
         }
+    db.collection("restaurants").document("restaurant2").collection("dishes").document(DataManagerDishes.dishes[position].documentId!!)
+
         finish()
 
 
