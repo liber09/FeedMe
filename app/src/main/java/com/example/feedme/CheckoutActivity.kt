571ba7fe4e2e9fd99ef97e2dccb979848686982a
liber09 <linda.bergsangel@hotmail.com>
2023-02-22 23:59:48 +0100

    Getting data into the order confirmation view

--- app/src/main/java/com/example/feedme/CheckoutActivity.kt
+++ app/src/main/java/com/example/feedme/CheckoutActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 
 class CheckoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,7 +13,10 @@ class CheckoutActivity : AppCompatActivity() {
 
         val btnCompleteOrder = findViewById<Button>(R.id.btnFinishOrder)
         btnCompleteOrder.setOnClickListener{
+            val arInfo: ArrayList<String> = ArrayList()
+            arInfo.add(findViewById<EditText>(R.id.editTextTextMultiLine2).text.toString())
             val intent = Intent(this, CustomerOrderConfirmationActivity::class.java)
+            intent.putStringArrayListExtra("list",arInfo)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/feedme/CustomerOrderConfirmationActivity.kt
+++ app/src/main/java/com/example/feedme/CustomerOrderConfirmationActivity.kt
@@ -1,10 +1,12 @@
 package com.example.feedme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.feedme.R
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 lateinit var RVCustomerOrderConfirmation : RecyclerView
 class CustomerOrderConfirmationActivity : AppCompatActivity() {
@@ -15,5 +17,31 @@ class CustomerOrderConfirmationActivity : AppCompatActivity() {
         RVCustomerOrderConfirmation.layoutManager= LinearLayoutManager(this)
         val adapter = CustomerOrderConfirmationRVAdapter(this,DataManagerShoppingCart.shoppingCartItems)
         RVCustomerOrderConfirmation.adapter = adapter
+        val ar1 = intent.extras!!.getStringArrayList("list")
+        val customerData = ar1?.get(0)?.split(" ")?.toMutableList()
+        customerData?.removeAll(listOf("",null))
+
+        val sb = StringBuilder()
+        sb.append(customerData?.get(0).toString()).append(" ").append(customerData?.get(1).toString())
+        val nameView = findViewById<TextView>(R.id.TVOrderConfirmationCustomerName)
+        nameView.text = sb.toString()
+        val addressView = findViewById<TextView>(R.id.TVOrderConfirmationCustomerAddress)
+        sb.clear()
+        sb.append(customerData?.get(2).toString()).append(" ").append(customerData?.get(3).toString())
+        if (customerData != null) {
+            addressView.text = sb.toString()
+        }
+        val postalCodeView = findViewById<TextView>(R.id.TVOrderConfirmationCustomerPostalCode)
+        if (customerData != null) {
+            postalCodeView.text = customerData.get(4).toString()
+        }
+        val cityView = findViewById<TextView>(R.id.TVOrderConfirmationCustomerCity)
+        if (customerData != null) {
+            cityView.text = customerData.get(5).toString()
+        }
+        val orderDateTime = findViewById<TextView>(R.id.TVOrderConfirmationCustomerOrderDate)
+        val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
+        val current = LocalDateTime.now().format(formatter)
+        orderDateTime.text = current.toString()
     }
 }
