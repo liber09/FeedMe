33d0109fe7374ef4202c37530c8213e5bf5d8b12
Julia Petersson <113100420+JuliaGloggPete@users.noreply.github.com>
2023-02-27 09:15:11 +0100

    You can add a drink to your restaurant

--- app/src/main/java/com/example/feedme/DrinksViewActivity.kt
+++ app/src/main/java/com/example/feedme/DrinksViewActivity.kt
@@ -5,13 +5,23 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.feedme.R.id.*
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class DrinksViewActivity : AppCompatActivity() {
 
     lateinit var drinksRecyclerView : RecyclerView
+    lateinit var auth: FirebaseAuth
+    lateinit var userUID : String
+    lateinit var restaurantid: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drinks_view)
@@ -26,6 +36,39 @@ class DrinksViewActivity : AppCompatActivity() {
         drinksRecyclerView.layoutManager= LinearLayoutManager(this)
         val adapter = DrinksViewRecyclerAdapter(this,DataManagerDrinks.drinkList)
         drinksRecyclerView.adapter = adapter
+
+        val fab_add_drink = findViewById<FloatingActionButton>(R.id.FAB_ADD_Drink)
+        fab_add_drink.isInvisible = true
+
+        auth = Firebase.auth
+        val user = auth.currentUser
+        userUID = user!!.uid.toString()
+        restaurantid = intent.getStringExtra("restId").toString()
+
+        if (user != null) {
+            val n =2
+            val userId = restaurantid.dropLast(n)
+
+            if (userUID == userId){
+                fab_add_drink.isVisible = true
+
+            } }
+
+
+        fab_add_drink.setOnClickListener{
+            val intent = Intent(this, AddNChangeDrinks::class.java)
+
+            intent.putExtra("resid", restaurantid)
+
+
+            startActivity(intent)
+
+
+        }
+
+
+
+
 
     }
 
